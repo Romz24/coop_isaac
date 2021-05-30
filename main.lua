65710bd0573a55637a14838aa1b177f5b3779289
Romzes <romz24@mail.ru>
2021-05-31 01:22:29 +0300

    Возможность менять цвета

--- main.lua
+++ main.lua
@@ -5,14 +5,20 @@ local CoopMod = RegisterMod(CoopName, 1)
 local CoopGame = Game()
 local CoopFont = Font()
 local CoopVersion = "1.0"
-local CoopColors = {
-	Character = {
-		Color(1.0, 0.4, 0.4),
-		Color(0.0, 0.4, 1.0),
-		Color(0.4, 0.8, 0.4),
-		Color(0.89, 0.8, 0.22),
-	},
+local CoopMaxPlayers = 4
+local CoopPlayers = {
+	Max = 4,
+	Character = { },
 	Name = { },
+}
+local CoopColors = {
+	{name = "Red", color = Color(1.0, 0.4, 0.4)},
+	{name = "Blue", color = Color(0.0, 0.4, 1.0)},
+	{name = "Green", color = Color(0.4, 0.8, 0.4)},
+	{name = "Yellow", color = Color(0.89, 0.8, 0.22)},
+	{name = "White", color = Color(0.9, 0.9, 0.9)},
+	{name = "Black", color = Color(0.1, 0.1, 0.1)},
+	{name = "Purple", color = Color(0.5, 0.0, 0.5)},
 }
 local CoopSettings = {
 	["ModEnable"] = true,
@@ -21,12 +27,6 @@ local CoopSettings = {
 	["ShowGhost"] = true,
 	["ButtonPressed"] = false,
 }
-
-for index, color in ipairs(CoopColors.Character) do
-	CoopColors.Name[index] = KColor(color.R, color.G, color.B, 0.4)
-end
-
-CoopFont:Load("font/pftempestasevencondensed.fnt")
 
 local function IsButtonPressed(players)
 	for i = 1, players do
@@ -38,6 +38,22 @@ local function IsButtonPressed(players)
 	end
 	
 	return false
+end
+
+local function UpdatePlayersColor()
+	for i = 1, CoopPlayers.Max do
+		local player = CoopSettings["PlayerColor" .. i] or i
+		local color = CoopColors[player].color
+		
+		CoopPlayers.Character[i] = color
+		CoopPlayers.Name[i] = KColor(color.R, color.G, color.B, 0.4)
+	end
+end
+
+local function OnModInit()
+	CoopFont:Load("font/pftempestasevencondensed.fnt")
+	UpdatePlayersColor()
+	print("Mod " .. CoopName .. " v" .. CoopVersion .. " loaded!")
 end
 
 function CoopMod:OnGameRender()
@@ -69,13 +85,13 @@ function CoopMod:OnGameRender()
 		if player ~= nil then
 			if player:IsCoopGhost() == false or CoopSettings["ShowGhost"] then
 				if CoopSettings["ShowColor"] then
-					player:SetColor(CoopColors.Character[i], 2, 100, false, false)
+					player:SetColor(CoopPlayers.Character[i], 2, 100, false, false)
 				end
 				
 				if (CoopFont:IsLoaded() and CoopSettings["ShowName"]) then
 					local position = Isaac.WorldToScreen(player.Position)
 					
-					CoopFont:DrawStringUTF8("P" .. i, position.X - 5, position.Y, CoopColors.Name[i])
+					CoopFont:DrawStringUTF8("P" .. i, position.X - 5, position.Y, CoopPlayers.Name[i])
 				end
 			end
 		end
@@ -97,6 +113,8 @@ if ModConfigLoaded then
 				CoopMod:RemoveData()
 			end
 		end
+		
+		OnModInit()
 	end
 	
 	function CoopMod:OnGameExit()
@@ -109,11 +127,11 @@ if ModConfigLoaded then
 	CoopMod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, CoopMod.OnGameExit)
 	
 	ModConfig.AddSpace(CoopName, "Info")
-	ModConfig.AddText(CoopName, "Info", function() return CoopName end)
+	ModConfig.AddText(CoopName, "Info", "Mod " .. CoopName)
 	ModConfig.AddSpace(CoopName, "Info")
-	ModConfig.AddText(CoopName, "Info", function() return "Version " .. CoopVersion end)
+	ModConfig.AddText(CoopName, "Info", "Version " .. CoopVersion)
 	ModConfig.AddSpace(CoopName, "Info")
-	ModConfig.AddText(CoopName, "Info", function() return "by Romzes" end)
+	ModConfig.AddText(CoopName, "Info", "by Romzes")
 	
 	ModConfig.AddSetting
 	(
@@ -226,4 +244,30 @@ if ModConfigLoaded then
 			end
 		}
 	)
+	
+	for i = 1, CoopPlayers.Max do
+		ModConfig.AddSetting
+		(
+			CoopName,
+			"Color",
+			{
+				Type = ModConfigMenu.OptionType.NUMBER,
+				CurrentSetting = function()
+					return CoopSettings["PlayerColor" .. i]
+				end,
+				Minimum = 1,
+				Maximum = #CoopColors,
+				Display = function()
+					return "Player " .. i .. ": " .. CoopColors[CoopSettings["PlayerColor" .. i]].name
+				end,
+				OnChange = function(currentNum)
+					CoopSettings["PlayerColor" .. i] = currentNum
+					UpdatePlayersColor()
+				end,
+				Info = "Change player color"
+			}
+		)
+	end
+else
+	OnModInit()
 end
